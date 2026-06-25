b0498889b1a604dc0690ca262e519714da265b8d
RABIUL HASSAN <rabiulhassandev@gmail.com>
2026-06-25 19:24:47 +0800

    remove product chart img

--- resources/views/products/show.blade.php
+++ resources/views/products/show.blade.php
@@ -185,11 +185,6 @@
                 <p class="text-xs text-gray-400 mt-2 text-center" x-show="!selectedSize">* সাইজ বেছে নিন</p>
             @endif
 
-            <!-- Product Chart Image -->
-            @php $chartImage = \App\Models\Setting::get('product_chart_image'); @endphp
-            <div class="mt-10">
-                <img src="{{ $chartImage ? asset('storage/' . $chartImage) : asset('assets/images/product-chart.jpeg') }}" alt="Product Chart" class="w-full rounded-lg border border-gray-200">
-            </div>
         </div>
     </div>
 
